89acb5aaca1c2950d312a7d9818333b781f65e87
Janoscikova, Andrea <andrea.janoscikova@rwe.cz>
2019-12-10 21:41:16 +0100

    fix server js

--- server.js
+++ server.js
@@ -4,17 +4,29 @@ const app = express();
 const { Client } = require('pg');
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/mashup-master-app'));
+app.use(express.static(__dirname + '/dist/ng-mashup'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/mashup-master-app/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/ng-mashup/index.html'));
 });
 
 // default Heroku PORT
-app.listen(process.env.PORT || 3000);
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
+app.listen(process.env.PORT || 3000, () => {
+  console.log('server running on' + process.env.PORT);
 });
+
+
+
+/**
+ * const http = require('http');
+const PORT = process.env.PORT || 5000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+server.listen(PORT, () => {
+  console.log(`Server running on ${PORT}/`);
+});
+ */
