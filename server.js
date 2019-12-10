5175396e1cb4fab2126ebfa020c57deaebfc0fb4
Janoscikova, Andrea <andrea.janoscikova@rwe.cz>
2019-12-10 20:13:25 +0100

    news changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const { Client } = require('pg');
 
 // Serve static files....
 app.use(express.static(__dirname + '/dist/mashup-master-app'));
@@ -12,3 +13,8 @@ app.get('/*', function(req, res) {
 
 // default Heroku PORT
 app.listen(process.env.PORT || 3000);
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
